654ae2ba25c5ea4e905b5d63a5b62936ce3a4cdf
Daniele Alessi <daniele.alessi@hotmail.it>
2021-03-22 16:12:34 +0100

    aggiunta ciclo per controllo utente

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Array Lista utenti di chi può accedere
+var listaUtenti = ["pippo@gmail.com", "pluto@gmail.com", "paperino@gmail.com", "topolino@gmail.com"];
+console.log(listaUtenti);
+
+
+Chiedi all’utente la sua email
+var mailUtente = prompt("Ciao, inserisci la tua email");
+console.log(mailUtente);
+
+
+// Controllare se l'utente fa parte della lista
+for (var i = 0; i < listaUtenti.length; i++) {
+    var mail = listaUtenti[i];
+
+    console.log(utenti);
+}
+
+
+
+
