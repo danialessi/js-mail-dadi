723789f5794b0a45b988fae94540eb6802c6ceb7
Daniele Alessi <daniele.alessi@hotmail.it>
2021-03-26 15:47:43 +0100

    aggiunto ciclo for

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+// Mail
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
 // Array Lista utenti di chi può accedere
 var listaUtenti = ["pippo@gmail.com", "pluto@gmail.com", "paperino@gmail.com", "topolino@gmail.com"];
 console.log('lista utenti:', listaUtenti);
@@ -6,12 +9,27 @@ console.log('lista utenti:', listaUtenti);
 var mailUtente = prompt("Ciao, inserisci la tua email");
 
 // Controllare se l'utente fa parte della lista
-for (var i = 0; i < listaUtenti.length; i++) {
-    var utenteIncluso = listaUtenti[i];
-    
-    console.log('utente incluso:', utenteIncluso);
+// ho bisogno della mail dell'utente (mailUtente) 
+// ho bisogno della lista (listaUtenti) 
+// devo confrontare la mail utente con la lista e trovare un risultato, salvo il risultato in una variabile vuota che poi andrò a popolare
 
-    if (mailUtente == utenteIncluso) {
-        alert("Perfetto " + mailUtente + " puoi giocare!")
+var risultatoConfronto;
+
+for (var i = 0; i < listaUtenti.length; i++) {
+    // mail controllata in questo momento del ciclo 
+    var thisEmail = listaUtenti[i];
+    // console.log(listaUtenti[i])
+
+    // poi il ciclo confronta la mail utente con quella controllata
+    if (thisEmail == mailUtente) {
+        risultatoConfronto = true;
+        alert("perfetto, puoi entrare");
+    } else {
+        risultatoConfronto = false;
+        alert("spiacente non sei nella lista");
     }
 }
+
+
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
